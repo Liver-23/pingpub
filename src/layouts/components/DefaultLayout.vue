5a53358a7f89ea857e7897dccb2da4839a252079
Liver-23 <liver23@gmail.com>
2024-02-13 16:20:59 +0200

    Fix icon size

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -341,7 +341,6 @@ function selected(route: any, nav: NavLink) {
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
-          <Icon icon="mdi:laptop" class="text-xl mr-2" />
           <img src="../../assets/cosmos.png" alt="Icon" class="text-xl mr-2" />
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
