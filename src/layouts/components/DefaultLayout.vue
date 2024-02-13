99047619335092f2fa222fe708dee60750d1d564
Liver-23 <liver23@gmail.com>
2024-02-13 16:17:45 +0200

    Add restake and cosmos directory links

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -321,7 +321,32 @@ function selected(route: any, nav: NavLink) {
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
-            Validator Services
+            Validator Docs
+          </div>
+        </a>
+        <a
+          href="https://cosmos.directory"
+          target="_blank"
+          class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
+        >
+        <img src="../../assets/cosmos.png" alt="Icon" class="text-xl mr-2" />
+          <div
+            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
+          >
+            Cosmos Directory
+          </div>
+        </a>
+        <a
+          href="https://restake.app"
+          target="_blank"
+          class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
+        >
+          <Icon icon="mdi:laptop" class="text-xl mr-2" />
+          <img src="../../assets/cosmos.png" alt="Icon" class="text-xl mr-2" />
+          <div
+            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
+          >
+            Restake.App
           </div>
         </a>
       </div>
